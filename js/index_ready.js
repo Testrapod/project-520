16b87b603c66acb704e15b4cfa6db6ee190e6e04
ddS2dd <lalasea2022@gmail.com>
2023-04-02 18:31:35 +0900

    시놉시스 완성

--- js/index_ready.js
+++ js/index_ready.js
@@ -1,4 +1,76 @@
 /* index.html document ready */
-$(document).ready(function () {
+current_idx = 0
+screen_list = [
+    "synopsis",
+    "question1",
+]
+answer_list = [
+    "520522",
+    "하울의 움직이는 성",
+]
 
+$(document).ready(function () {
+    ////////////////////////////////////////////////////////////////////////////////
+    // BGM
+    var bgm; var bgmOn = false; var bgmFirstTime = true;
+    $("#bgm_button").click(function () {
+        if (!bgmOn) {
+            if (bgmFirstTime) {
+                // bgm = new Audio("bgm/ghibli.mp3");
+                bgmFirstTime = false;
+            }
+            // bgm.play();
+            bgmOn = true;
+
+            $("#bgm_button").removeClass("bi-volume-up");
+            $("#bgm_button").addClass("bi-volume-mute");
+        } else {
+            // bgm.pause();
+            bgmOn = false;
+
+            $("#bgm_button").removeClass("bi-volume-mute");
+            $("#bgm_button").addClass("bi-volume-up");
+        }
+    });
+    ////////////////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Enter
+    function handleEnter() {
+        var answer = $("#answer_input").val();
+        $("#answer_input").val("");
+        
+        if (answer != answer_list[current_idx]) {
+            $("#alert_message").text("잘못 입력하셨습니다.");
+            $("#alert_fail").show();
+            setTimeout(function () { $("#alert_fail").fadeOut(); }, 1000);
+            return;
+        }
+        
+        current_idx += 1;
+        showScreen(screen_list[current_idx]);
+
+        $("#main_screen").hide();
+        $("#main_screen").fadeIn(2000);
+    }
+    $("#answer_input").on("keyup", function (key) {
+        if (key.keyCode == 13) handleEnter();
+    });
+    $("#enter_button").click(handleEnter);
+    ////////////////////////////////////////////////////////////////////////////////
+
+    var typedTitle = new Typed("#title", {
+        strings: ["PROJECT: 520"],
+        typeSpeed: 120,
+        backSpeed: 120,
+        loop: true,
+        showCursor: false,
+    });
+
+    function showScreen(page) {
+        src = "./screen/" + page + ".html";
+        $("#main_screen").attr("src", src);
+    }
+
+    showScreen("synopsis");
 });
---
+++ js/synopsis_ready.js
@@ -0,0 +1,40 @@
+/* synopsis.html document ready */
+$(document).ready(function () {
+    synopsis = `
+        <h4 class="mb-3">시놉시스</h4>
+        작년 <span class="yuntted-text">윤티드</span>에서 처음 만난 동해와 유나은은 서로의 마음을 확인하고 연애를 시작하게 되었다! ❤️<br/>
+        행복하게 연애한지 어느덧 1주년,, 기다리고 기다리던 유나은의 생일이 다가왔다..!<br/><br/>
+        동해는 작년에 받은 이벤트에 감동해 올해는 더 멋진(?) 이벤트를 하겠다고 다짐했는데..<br/>
+        이를 몰래 지켜보던 유나은은 동해가 어떤 이벤트를 준비하고 있는지 궁금해졌다! 😄<br/><br/>
+        그래서 동해가 자고있는 틈을 타 동해의 노트북을 뒤져보았는데..<br/>
+        이상한 폴더를 하나 발견했다..! 🫢 폴더의 이름은<br/>
+        <span class="large-text">Project 520</span>..?<br/>
+        무슨 폴더지? 우선 한번 확인해볼까~? 😆
+    `
+    help = `
+        <h4 class="mb-3">💻 컴탈출 방법 💻</h4>
+        1. 플레이어(유나은)는 <span class="gray-text">파일들에 걸린 암호</span>를 풀어내며 동해의 이벤트를 알아내는 것이 목표!<br/>
+        2. <span class="gray-text">파일에 걸린 암호</span>는 동해도 까먹을까봐 문제 형태로 풀 수 있게 되어있다. 😅<br/>
+        3. 파일은 총 <span class="large-text">16</span>개! 즉 <span class="large-text">16</span>개의 문제를 모두 풀어야만 맨 마지막에 동해의 이벤트를 알아낼 수 있다.<br/>
+        4. <span class="gray-text">암호</span>를 알아내면 하단 입력창에 입력하자. 그러면 자동으로 다음 파일로 넘어간다.<br/>
+        5. 문제는 방탈출처럼 다소 친절하지(?) 않을 수 있다..! 대신 각 문제마다 힌트가 있으니 참고하도록.<br/>
+        (너무 많은 힌트 사용은 탈출의 재미를 반감시킬 수 있으니 주의바람 ⚠️)<br/><br/>
+        우선 연습삼아 컴퓨터 암호부터 풀어보자.<br/>
+        컴퓨터 암호도 핸드폰 암호랑 같았던거 같은데.. 🤔
+    `
+
+    $("#content").html(synopsis);
+
+    isSynopsis = true;
+    $("#next_button").click(function () {
+        if (isSynopsis) {
+            $("#content").html(help);
+            $("#next_button").html("이전")
+            isSynopsis = false;
+        } else {
+            $("#content").html(synopsis);
+            $("#next_button").html("다음")
+            isSynopsis = true;
+        }
+    });
+});
